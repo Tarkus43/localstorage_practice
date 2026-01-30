64639e40321aa10f556bdf59cd4805bbd6e4ba46
Yevhenii Baibikov <jeka.spets333@gmail.com>
2026-01-30 14:59:48 +0100

    renamed some classes and made logic for submit button

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const btn = document.getElementById('create_btn')
 const list = document.getElementById('todoList')
-const form = document.getElementById('todoForm')
+const formWrapper = document.getElementById('todoFormWrapper')
+const form = document.querySelector('#todoForm')
 
 btn.addEventListener('click', () => {
-    form.classList.toggle('hidden')
+    formWrapper.classList.toggle('hidden')
     btn.textContent = btn.textContent == '+' ? btn.textContent = 'x' : btn.textContent = '+'
 })
+
+form.elements.submit.addEventListener('click',(event) => {
+    event.preventDefault()
+    const todoTitle = form.elements.titleForm.value
+    const todoText = form.elements.descriptionForm.value
+
+    const todoObject = {
+        
+    }
+})
+
+console.log(form.elements.submit)
+
