7d8b0376037e840f4d14b029afce8d2815759058
Yevhenii Baibikov <jeka.spets333@gmail.com>
2026-03-24 17:31:58 +0100

    replaced renderTodos func to crud file

--- src/crud.js
+++ src/crud.js
@@ -5,3 +5,28 @@ const addNewTodo = (todo) => {
     console.log(todoList)
     localStorage.setItem('todos', JSON.stringify(todoList))
 }
+
+const renderTodos = () => {
+    const list = document.getElementById('todoList')
+
+    list.innerHTML = ''
+
+    const todoList = Object.entries(JSON.parse(localStorage.getItem('todos')))
+
+    for (let i = 1; i < todoList.length; i++) {
+        const li = document.createElement('li')
+
+        const title = document.createElement('h3')
+        title.textContent = todoList[i][1]['title']
+
+        const text = document.createElement('p')
+        text.textContent = todoList[i][1]['text']
+
+        li.appendChild(title)
+        li.appendChild(text)
+
+
+        list.appendChild(li)
+    }
+
+}
--- src/helpers.js
+++ src/helpers.js
@@ -6,37 +6,13 @@ const findLastId = () => {
 
 const checkIfTodoEmpty = () => {
     if (localStorage.getItem('todos') == null){
-    localStorage.setItem('todos', JSON.stringify(
-        {
-            0: 'YOU SHOULD NOT SEE THIS'
-        }
-    ))
-}
-}
-
-const renderTodos = () => {
-    const list = document.getElementById('todoList')
-
-    list.innerHTML = ''
-
-    const todoList = Object.entries(JSON.parse(localStorage.getItem('todos')))
-
-    for (let i = 1; i < todoList.length; i++) {
-        const li = document.createElement('li')
-
-        const title = document.createElement('h3')
-        title.textContent = todoList[i][1]['title']
-
-        const text = document.createElement('p')
-        text.textContent = todoList[i][1]['text']
-
-        li.appendChild(title)
-        li.appendChild(text)
-
-
-        list.appendChild(li)
+        localStorage.setItem('todos', JSON.stringify(
+            {
+                0: 'YOU SHOULD NOT SEE THIS'
+            }
+        ))
     }
-
 }
+
 
 export {findLastId, addNewTodo, checkIfTodoEmpty, renderTodos}
